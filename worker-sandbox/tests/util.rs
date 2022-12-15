0b7989eafc75299b5b7d76165f5aab6d22b73018
Jonathan Donaldson <jon@than.dev>
2022-12-15 18:40:44 +0000

    Fix formatting

--- worker-sandbox/tests/util.rs
+++ worker-sandbox/tests/util.rs
@@ -64,10 +64,8 @@ pub fn options(
     builder_fn: impl FnOnce(RequestBuilder) -> RequestBuilder,
 ) -> Response {
     expect_wrangler();
-    let builder = Client::new().request(
-        Method::OPTIONS,
-        format!("http://127.0.0.1:8787/{endpoint}"),
-    );
+    let builder =
+        Client::new().request(Method::OPTIONS, format!("http://127.0.0.1:8787/{endpoint}"));
     let builder = builder_fn(builder);
 
     builder
